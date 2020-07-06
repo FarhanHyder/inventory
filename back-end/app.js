ee3ddbaa9e7f00e1ab11746dee45a1f8663a612f
Farhan Hyder <fmshyder@gmail.com>
2020-07-06 11:50:34 -0400

    DB connection stablished

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 
 // routes
 app.get('/', (req,res) =>{
     res.send('this is home');
 })
+
+
+// connect to mongoDB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true}, 
+    () => console.log('Connected to DB')
+    );
 
 
 app.listen(3000);
